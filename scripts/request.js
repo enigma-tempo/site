94bc79d00f88947a9a8a45129781910b3c65fd34
diogoasp <diogoasp13@gmail.com>
2023-03-26 17:34:33 -0300

    uploadimage fix

--- scripts/request.js
+++ scripts/request.js
@@ -19,16 +19,17 @@ function postRequest(url, json) {
     resolve(request);
   });
 }
-function uploadImage(file){
-  const formdata = new FormData()
-  formdata.append("image", file.target.files[0])
-  fetch("https://api.imgur.com/3/image/", {
-      method: "post",
-      headers: {
-          Authorization: "Client-ID b37dc6409000178"
-      },
-      body: formdata
-  }).then(data => data.json()).then(data => {
-      return data.data.link;
-  })
+
+function uploadImage(fileInput){
+  files = fileInput.files[0];
+  const token = {}
+  const client = filestack.init("AzheqOZy3Txuu4dgJPuRJz");
+
+  client.upload(files, {}, {}, token)
+    .then(res => {
+      return res.url;
+    })
+    .catch(err => {
+      console.log(err)
+    });
 }
