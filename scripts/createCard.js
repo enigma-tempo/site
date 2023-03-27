7bae91920c2a67487c522fcdcf06ebb91e0a4203
Diogo Alves <diogoasp13@gmail.com>
2023-03-27 15:11:58 -0300

    new effects on list/game setup only logged

--- scripts/createCard.js
+++ scripts/createCard.js
@@ -1,91 +1,9 @@
-// var effectDefault = {'effects':
-//                     [
-//                         {
-//                             'id': 1,
-//                             'name': 'Inspirar',
-//                             'effect': 'buff',
-//                             'params': 'alieds:number,attack:number,health:number,posture:text',
-//                             'description': 'Inspira um ou mais aliados aumentando o valor de ataque e vida deles'
-//                         },
-//                         {
-//                             'id': 2,
-//                             'name': 'Provocar',
-//                             'effect': 'taunt',
-//                             'params': '',
-//                             'description': 'Inimigos só podem atacar esta carta'
-//                         },
-//                         {
-//                             'id': 3,
-//                             'name': 'Invocar',
-//                             'effect': 'summon',
-//                             'params': 'card_name:text,quantity:number',
-//                             'description': 'Invoca uma ou mais cartas para o campo'
-//                         },
-//                         {
-//                             'id': 4,
-//                             'name': 'Comprar carta',
-//                             'effect': 'draw',
-//                             'params': 'quantity:number',
-//                             'description': 'Compra cartas para sua mão'
-//                         },
-//                         {
-//                             'id': 5,
-//                             'name': 'Escudo',
-//                             'effect': 'divineshield',
-//                             'params': '',
-//                             'description': 'Bloqueia o próximo ataque'
-//                         },
-//                         {
-//                             'id': 6,
-//                             'name': 'Agilidade',
-//                             'effect': 'charge',
-//                             'params': '',
-//                             'description': 'Pode atacar imediatamente'
-//                         },
-//                         {
-//                             'id': 7,
-//                             'name': 'Causar dano a personalidade inimiga',
-//                             'effect': 'dealDamageHero',
-//                             'params': 'damage:number',
-//                             'description': 'Causa dano direto à personalidade inimiga'
-//                         },
-//                         {
-//                             'id': 8,
-//                             'name': 'Curar personalidade aliada',
-//                             'effect': 'healHero',
-//                             'params': 'heal:number',
-//                             'description': 'Cura sua personalidade aliada'
-//                         },
-//                         {
-//                             'id': 9,
-//                             'name': 'Mudar vida da personalidade inimiga',
-//                             'effect': 'setAttib',
-//                             'params': 'value:number',
-//                             'description': 'Muda a vida da personalidade inimiga para o valor escolhido'
-//                         },
-//                         {
-//                             'id': 10,
-//                             'name': 'Motivação',
-//                             'effect': 'buffSelf',
-//                             'params': 'attack:number,health:number',
-//                             'description': 'Aumenta o valor de ataque e vida de si mesmo.'
-//                         },
-//                         {
-//                             'id': 11,
-//                             'name': 'Intimidar',
-//                             'effect': 'damageEnemies',
-//                             'params': 'enemies:number,health:number',
-//                             'description': 'Causa dano em um ou mais inimigos em campo.'
-//                         }
-//                     ]
-//                 };
-
 if (sessionStorage.getItem('role') !== 'admin') {
     window.location.href = 'cartas.html';
 }
 
 exclude_list = ['attackEnemy'];
-const paramsNames = {'alieds':'Quantidade de aliados','attack':'Ataque','health':'Vida','card_name':'Nome da carta','quantity':'Quantidade','damage':'Valor do dano','heal':'Valor da cura','value':'Valor','enemies':'Quantidade de inimigos','posture':'Postura afetada'};
+const paramsNames = {'alieds':'Quantidade de aliados','attack':'Ataque','health':'Vida','card_name':'Nome da carta','quantity':'Quantidade','damage':'Valor do dano','heal':'Valor da cura','value':'Valor','enemies':'Quantidade de inimigos','posture':'Postura afetada', 'acting': 'Atuação'};
 
 var itens = document.getElementsByClassName("update");
 const name = document.getElementById('name');
@@ -146,7 +64,7 @@ function showParams(name){
     effectList.effects.forEach(element => {
         if (element.name == name) {
             paramsDefault = element.params;
-            document.getElementById("effectDescription").innerText = element.description;
+            document.getElementById("effectDescription").innerHTML = element.description;
         }
     });
     if (paramsDefault != "") {
