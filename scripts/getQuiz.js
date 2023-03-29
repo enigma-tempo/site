963db2f8e3c2a045bdfb752ee5db58ceda63fa2e
Diogo Alves <diogoasp13@gmail.com>
2023-03-29 20:37:36 -0300

    getQuiz v1/new icons

---
+++ scripts/getQuiz.js
@@ -0,0 +1,67 @@
+if (sessionStorage.getItem('role') !== 'admin') {
+  document.getElementById('criar_personalidade_btn').remove();
+}
+if (sessionStorage.getItem('token') === null || sessionStorage.getItem('token') === '') {
+  window.location.href = 'login.html';
+}
+
+$(function () {
+  document.getElementById('play_quiz').addEventListener('click', startQuiz);
+});
+
+function startQuiz() {
+  var questions = [];
+  var pontuação = 0;
+  getQuestions(questions);
+  document.getElementById('inicio').classList.add('d-none');
+  document.getElementById('quiz').classList.remove('d-none');
+
+  questions.forEach(async (q, index) => {
+    //define a pergunta
+    document.getElementById('pergunta').innerHTML = q.question;
+    //define o tema
+    document.getElementById('tema').innerHTML = q.theme;
+    q.options.split(';').forEach((o, index) => {
+      document.getElementById(index + '-option').innerHTML = o;
+    });
+    let proximo = (document.getElementById('responder').innerHTML = q.theme);
+    proximo.addEventListener('click', () => {
+      pontuação += document.querySelector('input[name="resposta"]:checked').value == q.answer ? 5 : 0;
+    });
+    timer = setTimeout(() => {
+      proximo.click();
+    }, 120000);
+    await buttonClick(proximo);
+    clearTimeout(timer);
+  });
+  document.getElementById('resultado').classList.remove('d-none');
+  document.getElementById('resultado-valor').innerHTML = pontuação;
+}
+
+async function getQuestions(questions) {
+  setTimeout(async function () {
+    let data = await getRequest(urlBase + 'quizzes');
+    if (data['message'] == 'Erro inesperado. Bad Request ') {
+      alert('Não foi possível conectar ao banco de dados... Tente novamente mais tarde');
+      return false;
+    }
+    let quizzes = data['quizzes'];
+
+    const N = lista.length;
+    const n_questions = 5;
+    let random_quiz_list = [];
+    for (let i = 0; i < n_questions; i++) {
+      let num;
+      do {
+        num = Math.floor(Math.random() * N);
+      } while (random_quiz_list.filter((n) => n === num).length >= 1);
+      random_quiz_list.push(num);
+    }
+
+    random_quiz_list.forEach((element) => {
+      let quiz = createQuiz(quizzes[element]);
+      questions.push(quiz);
+    });
+    loading.classList.add('d-none');
+  }, 500);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,17 @@
 const urlBase = 'https://api-enigma-tempo.onrender.com/api/';
-const raritiesOptions = {'6420514b3407ebcfe7c8bc01':'comum','642051513407ebcfe7c8bc03':'raro','642051683407ebcfe7c8bc05':'epico','6420516f3407ebcfe7c8bc07':'lendario'};
+const raritiesOptions = { '6420514b3407ebcfe7c8bc01': 'comum', '642051513407ebcfe7c8bc03': 'raro', '642051683407ebcfe7c8bc05': 'epico', '6420516f3407ebcfe7c8bc07': 'lendario' };
 
 $(function () {
-    $('#header').load('header.html');
-    $('#footer').load('footer.html');
-  setTimeout(function(){
+  $('#header').load('header.html');
+  $('#footer').load('footer.html');
+  setTimeout(function () {
     if (sessionStorage.getItem('user') != null) {
       document.getElementById('jogar').classList.remove('d-none');
       document.getElementById('login').classList.add('d-none');
       document.getElementById('logout').classList.remove('d-none');
+      document.getElementById('to_quiz').classList.remove('d-none');
     }
-  },100);
+  }, 100);
 });
 
 function showAlert(id, tipo, titulo, mensagem) {
@@ -22,10 +23,7 @@ async function buttonClick(btn) {
   return new Promise((resolve) => (btn.onclick = () => resolve()));
 }
 
-
 function logout() {
   sessionStorage.clear();
   window.location.href = 'login.html';
 }
-
-
